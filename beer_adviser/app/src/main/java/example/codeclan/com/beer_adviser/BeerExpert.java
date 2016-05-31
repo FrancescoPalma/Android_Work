b113ead3fd8288722982ac38bd229d4717b793c4
Francesco <fpfrancescopalma7@gmail.com>
2016-05-31 21:12:03 +0100

    A few fixes

--- beer_adviser/app/src/main/java/example/codeclan/com/beer_adviser/BeerExpert.java
+++ beer_adviser/app/src/main/java/example/codeclan/com/beer_adviser/BeerExpert.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class BeerExpert {
     List<String> getBrands(String color) {
-        List<String> brands = new ArrayList<String>();
+        List<String> brands = new ArrayList<>();
 
         if (color.equals("blonde")) {
             brands.add("Amstel");
