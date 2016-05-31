c8f43d5a98c5692ea636ebc08e49adb56cdd05a0
Francesco <fpfrancescopalma7@gmail.com>
2016-05-31 20:30:01 +0100

    Adds initial version of beer adviser app

---
+++ beer_adviser/app/src/main/java/example/codeclan/com/beer_adviser/BeerExpert.java
@@ -0,0 +1,27 @@
+package example.codeclan.com.beer_adviser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by user on 31/05/2016.
+ */
+public class BeerExpert {
+    List<String> getBrands(String color) {
+        List<String> brands = new ArrayList<String>();
+
+        if (color.equals("blonde")) {
+            brands.add("Amstel");
+        }
+
+        if (color.equals("dark")) {
+            brands.add("Guinness");
+        }
+
+        if (color.equals("red")) {
+            brands.add("Red Moose");
+        }
+
+        return brands;
+    }
+}
